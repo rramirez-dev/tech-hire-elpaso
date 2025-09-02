308915b55f3f1a98e77eb99d1708139ee2add5c9
Robert Ramirez <robertpramirez44@gmail.com>
2025-09-01 20:50:49 -0600

    Installed NextJS
    -Added stub for Header Component

---
+++ tech-hire-elpaso/src/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <>
+      <h1 className="text-5xl front-bold text-pink-800 mb4">
+        Appwrite 2025 Hackathon
+      </h1>
+      <h5 className="text-2xl font-bold text-blue-800 mb4">
+        Tech Hire El Paso
+      </h5>
+    </>
+  );
+}
