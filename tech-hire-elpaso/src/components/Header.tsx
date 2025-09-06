3518834ef27517cf7a928e9bc3330809cd4a2ceb
Robert Ramirez <robertpramirez44@gmail.com>
2025-09-06 12:06:45 -0600

    Implemented responsive navbar
    -Added site logo
    -Removed boiler plate code
    -Added padding to main content

--- tech-hire-elpaso/src/components/Header.tsx
+++ tech-hire-elpaso/src/components/Header.tsx
@@ -1,5 +1,63 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
+import Image from "next/image";
 
 export default function Header() {
-  return <div>Header</div>;
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  return (
+    <>
+      <header className="flex justify-between items-center text-black py-4 px-4 md:px-32 bg-white drop-shadow-sm">
+        <a href="#">
+          <Image
+            src="/images/techhireep_logo.png"
+            alt="Tech Hire El Paso Logo"
+            width={100}
+            height={100}
+          ></Image>
+        </a>
+        <ul className="hidden xl:flex items-center gap-12 font-semibold text-base">
+          <li className="p-3  hover:text-red-500 rounded-md transition-all">
+            Home
+          </li>
+          <li className="p-3  hover:text-red-500 rounded-md transition-all">
+            Events
+          </li>
+          <li className="p-3  hover:text-red-500 rounded-md transition-all">
+            Contact
+          </li>
+        </ul>
+        <div className="relative hidden xs:flex sm:flex md:flex items-center justify-center gap-3">
+          <button className="hidden xl:block">Login</button>
+          <Image
+            className="xl:hidden text-5xl cursor-pointer block"
+            src="/images/menu.png"
+            width={30}
+            height={30}
+            alt="Menu"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+          ></Image>
+        </div>
+        {/* Mobile Menu */}
+        <div
+          className={`absolute xl:hidden top-22 left-0 w-full bg-white flex flex-col items-center gap-2 font-semibold text-lg transform transition-transform ${
+            isMenuOpen ? "opacity-100" : "opacity-0"
+          }`}
+          style={{ transition: "transform 0.3s ease,opacity 0.3s ease" }}
+        >
+          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
+            Home
+          </li>
+          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
+            Events
+          </li>
+          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
+            Contact
+          </li>
+          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
+            <button>Login</button>
+          </li>
+        </div>
+      </header>
+    </>
+  );
 }
