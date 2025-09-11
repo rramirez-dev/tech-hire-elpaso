bd40600d6c9c8ae1875f0e1e0d65c0d403b69f59
Robert Ramirez <robertpramirez44@gmail.com>
2025-09-10 21:36:30 -0600

    Implemented Footer Component
    -Added a footer for compy right info

--- tech-hire-elpaso/src/app/layout.tsx
+++ tech-hire-elpaso/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import Header from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { Poppins } from "next/font/google";
 import "./globals.css";
 
@@ -18,6 +19,7 @@ export default function RootLayout({
       <body>
         <Header />
         {children}
+        <Footer />
       </body>
     </html>
   );
---
+++ tech-hire-elpaso/src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Footer = () => {
+  return (
+    <div className="max-w-[1440px] container mx-auto px-20 py-5 rounded-xl bg-gray-200">
+      &copy; 2025 Tech Hire El Paso, All Rights Reserved
+    </div>
+  );
+};
