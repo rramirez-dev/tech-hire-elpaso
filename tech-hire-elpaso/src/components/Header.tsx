7e3bd51f99c7900af440525063e856a20d0a5157
Elsam Atchole <83823820+unkwnownGriot@users.noreply.github.com>
2025-10-19 14:56:47 +0000

    Add mobile menu links (Home, Events, Contact) and close menu on click (#29)
    * Add mobile menu links (Home, Events, Contact) and close menu on click
    
    * wrap mobile menu links in <li> and <ul> and correct padding class
    
    * wrap the ul tag inside a div in the mobile menu header section

--- tech-hire-elpaso/src/components/Header.tsx
+++ tech-hire-elpaso/src/components/Header.tsx
@@ -20,11 +20,11 @@ export default function Header() {
           <li className="p-3  hover:text-red-500 rounded-md transition-all">
             <Link href="/">Home</Link>
           </li>
-          <li className="p-3  hover:text-red-500 rounded-md transition-all">
-            Events
+          <li className="p-3 hover:text-red-500 rounded-md transition-all">
+            <Link href="/events">Events</Link>
           </li>
-          <li className="p-3  hover:text-red-500 rounded-md transition-all">
-            Contact
+          <li className="p-3 hover:text-red-500 rounded-md transition-all">
+            <Link href="/contact">Contact</Link>
           </li>
         </ul>
         <div className="relative xs:flex sm:flex md:flex items-center justify-center gap-3">
@@ -44,29 +44,42 @@ export default function Header() {
           ></Image>
         </div>
         {/* Mobile Menu */}
-        <div
-          className={`absolute xl:hidden top-22 left-0 w-full bg-white flex flex-col items-center gap-2 font-medium text-md transform transition-transform p-5 ${
-            isMenuOpen ? "opacity-100" : "opacity-0"
-          }`}
-          style={{ transition: "transform 0.3s ease,opacity 0.3s ease" }}
-        >
-          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
-            Home
-          </li>
-          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
-            Events
-          </li>
-          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
-            Contact
-          </li>
-          <li className="list-none w-full text-center p2 hover:text-red-500 transition-all cursor-pointer">
-            <Link
-              href="/login"
-              className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 mb-5"
-            >
-              Login
-            </Link>
-          </li>
+        <div>
+          <ul
+            className={`absolute xl:hidden top-22 left-0 w-full bg-white flex flex-col items-center gap-2 font-medium text-md transform transition-transform p-5 ${
+              isMenuOpen ? "opacity-100" : "opacity-0"
+            }`}
+            style={{ transition: "transform 0.3s ease,opacity 0.3s ease" }}
+          >
+            <li className="list-none w-full text-center p-2 hover:text-red-500 transition-all cursor-pointer">
+              <Link
+                href="/"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="list-none w-full text-center p-2 hover:text-red-500 transition-all cursor-pointer"
+              >
+                Home
+              </Link>
+            </li>
+            <li className="list-none w-full text-center p-2 hover:text-red-500 transition-all cursor-pointer">
+              <Link href="/events" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                Events
+              </Link>
+            </li>
+            <li className="list-none w-full text-center p-2 hover:text-red-500 transition-all cursor-pointer">
+              <Link href="/contact" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                Contact
+              </Link>
+            </li>
+            <li className="list-none w-full text-center p-2 hover:text-red-500 transition-all cursor-pointer">
+              <Link
+                href="/login"
+                onClick={() => setIsMenuOpen(!isMenuOpen)}
+                className="bg-red-500 text-white px-4 py-2 rounded-lg hover:bg-red-600 mb-5"
+              >
+                Login
+              </Link>
+            </li>
+          </ul>
         </div>
       </header>
     </>
