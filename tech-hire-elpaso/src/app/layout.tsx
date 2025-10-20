4d4fc97501a901eb35c70041a97eb79d43c9053f
Robert Ramirez <robertpramirez44@gmail.com>
2025-10-20 17:20:49 -0600

    Revert "Add dark mode classes to components (#31)"
    This reverts commit 8ca91f08091ac540dae02a69357ddf25e200bdff.

--- tech-hire-elpaso/src/app/layout.tsx
+++ tech-hire-elpaso/src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="dark:bg-black">
+      <body>
         <Header />
         {children}
         <Footer />
